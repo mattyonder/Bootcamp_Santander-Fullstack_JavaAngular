e579acd1cc53b5fbae68149a66584e50ece1b07c
mattyonder <mateussantosor5002@gmail.com>
2023-10-17 11:22:56 -0300

    Modulo Services

---
+++ Trabalhando com Rotas e Serviços no Angular/Trabalhando com Services/services/src/app/components/card/card.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  pokemon: PokemonData;
+
+  constructor(
+    private service: PokemonService
+  ) {
+
+    this.pokemon = {
+      id : 0,
+      name : '',
+      sprites : {
+        front_default: ''
+      },
+      types: []
+      }
+  }
+
+  ngOnInit(): void {
+    this.getPokemon("pikachu")
+
+  }
+
+  getPokemon(searchName: string) {
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (res) => {
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+        },
+        error: (err) => console.log('Not found')
+      }
+    )
+  }
+
+}
