98762ea299db11ea9f73b2e6579a9ebfa8c0da04
mattyonder <mateussantosor5002@gmail.com>
2023-10-20 21:19:09 -0300

    Save Point

---
+++ Ganhando Produtividade com Spring e Java/Criando uma API REST Documentada com Spring Web e Swagger/my-first-web-api/src/main/java/dio/my/web/api/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package dio.my.web.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public String welcome(){
+        return "Welcome to a Spring Boot REST API";
+    }
+}
