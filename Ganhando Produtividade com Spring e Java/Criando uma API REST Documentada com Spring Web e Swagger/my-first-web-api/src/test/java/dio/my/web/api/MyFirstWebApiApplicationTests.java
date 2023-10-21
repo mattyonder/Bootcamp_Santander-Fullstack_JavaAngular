78b459c2d4823c9ed4da3640b930bce96c04e98a
mattyonder <mateussantosor5002@gmail.com>
2023-10-20 21:58:38 -0300

    Deu certo

---
+++ Ganhando Produtividade com Spring e Java/Criando uma API REST Documentada com Spring Web e Swagger/my-first-web-api/src/test/java/dio/my/web/api/MyFirstWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.my.web.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
